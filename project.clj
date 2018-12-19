eba69909adb64e3abfa4000ca6d702b33723d879
Ludvig Sundström <lud.sund@gmail.com>
2018-12-19 11:54:17 +0100

    Add glob->regex and all

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject util "0.1.0-SNAPSHOT"
+(defproject me.lsund/util "0.2.0"
   :description "Personal utilities"
-  :url "http://example.com/FIXME"
+  :url "https://clojars.org/me.lsund/util"
   :dependencies [[org.clojure/clojure "1.8.0"]])
--- src/me/lsund/util.clj
+++ src/me/lsund/util.clj
@@ -2,3 +2,30 @@
 
 (defmacro .+ [& rest]
   `((comp ~@(butlast rest)) ~(last rest)))
+
+; taken from https://github.com/raynes/fs/blob/master/src/me/raynes/fs.clj
+(defn glob->regex
+  "takes a glob-format string and returns a regex."
+  [s]
+  (loop [stream s
+         re ""
+         curly-depth 0]
+    (let [[c j] stream]
+      (cond
+        (nil? c) (re-pattern ; we add ^ and $ since we check only for file names
+                  (str "^" (if (= \. (first s)) "" "(?=[^\\.])") re "$"))
+        (= c \\) (recur (nnext stream) (str re c c) curly-depth)
+        (= c \/) (recur (next stream) (str re (if (= \. j) c "/(?=[^\\.])"))
+                        curly-depth)
+        (= c \*) (recur (next stream) (str re "[^/]*") curly-depth)
+        (= c \?) (recur (next stream) (str re "[^/]") curly-depth)
+        (= c \{) (recur (next stream) (str re \() (inc curly-depth))
+        (= c \}) (recur (next stream) (str re \)) (dec curly-depth))
+        (and (= c \,) (< 0 curly-depth)) (recur (next stream) (str re \|)
+                                                curly-depth)
+        (#{\. \( \) \| \+ \^ \$ \@ \%} c) (recur (next stream) (str re \\ c)
+                                                 curly-depth)
+        :else (recur (next stream) (str re c) curly-depth)))))
+
+(defn all [xs]
+  (every? true? xs))
