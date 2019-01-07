fecd6831299f64246184490821da1e3599cab329
Ludvig Sundström <lud.sund@gmail.com>
2019-01-07 12:42:51 +0100

    Release version 0.3.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject me.lsund/util "0.2.0"
+(defproject me.lsund/util "0.3.0"
   :description "Personal utilities"
   :url "https://clojars.org/me.lsund/util"
   :dependencies [[org.clojure/clojure "1.8.0"]])
