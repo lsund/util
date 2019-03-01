c332062e730e9e514a6d0f3f2d022029cf942836
Ludvig Sundström <lud.sund@gmail.com>
2019-03-01 18:08:36 +0100

    Fix dependency problem

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
-(defproject me.lsund/util "0.4.0"
+(defproject me.lsund/util "0.4.1"
   :description "Personal utilities"
   :url "https://clojars.org/me.lsund/util"
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [compojure "1.6.1"]])
--- src/me/lsund/routes.clj
+++ src/me/lsund/routes.clj
@@ -1,24 +1,25 @@
 (ns me.lsund.routes
   (:require [clojure.edn :as edn]
-            [me.lsund.util :refer [select-keys-with-nil]]))
+            [me.lsund.util :refer [select-keys-with-nil]]
+            [compojure.core :as compojure]))
 
 (defmacro generate-routes [routes-file & xs#]
   (let [route-spec# (edn/read-string (slurp routes-file))]
-    `(routes
+    `(compojure/routes
       ~@(for [[method path args & body] xs#]
           (case method
-            get-route `(GET ~(if (keyword path)
-                               (get (:get route-spec#) path)
-                               (get-in (:get route-spec#) path))
-                            request-map#
-                            (do
-                              ((fn [{:keys ~args}] ~@body)
-                               (select-keys-with-nil (:params request-map#)
-                                                     ~(mapv keyword args)))))
-            post-route `(POST ~(if (keyword path)
-                                 (get (:post route-spec#) path)
-                                 (get-in (:post route-spec#) path))
-                              request-map#
-                              ((fn [{:keys ~args}] ~@body)
-                               (select-keys-with-nil (:params request-map#)
-                                                     ~(mapv keyword args)))))))))
+            get-route `(compojure/GET ~(if (keyword path)
+                                         (get (:get route-spec#) path)
+                                         (get-in (:get route-spec#) path))
+                                      request-map#
+                                      (do
+                                        ((fn [{:keys ~args}] ~@body)
+                                         (select-keys-with-nil (:params request-map#)
+                                                               ~(mapv keyword args)))))
+            post-route `(compojure/POST ~(if (keyword path)
+                                           (get (:post route-spec#) path)
+                                           (get-in (:post route-spec#) path))
+                                        request-map#
+                                        ((fn [{:keys ~args}] ~@body)
+                                         (select-keys-with-nil (:params request-map#)
+                                                               ~(mapv keyword args)))))))))
