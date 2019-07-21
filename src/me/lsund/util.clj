91566b7ccea11a92da9f1ef3b89d153eaf09e37c
Ludvig Sundström <lud.sund@gmail.com>
2019-07-21 08:48:38 +0200

    Add flip and const

--- src/me/lsund/util.clj
+++ src/me/lsund/util.clj
@@ -1,23 +1,41 @@
 (ns me.lsund.util
   (:require [clojure.set :as set]))
 
-(defn select-keys-with-nil [m ks]
-  "The result of [[(select-keys m ks)]] but if an element is present in ks but not in m,
-   add it with value nil. Order is not necessarily retained.
+(defn all [xs]
+  (every? true? xs))
+
+(defn any [xs]
+  (some true? xs))
+
+(defn- const
+  "Always return the first argument, discard the rest."
+  [x & _]
+  x)
+
+(defn flip [f x y]
+  (f y x))
+
+(defn select-keys-with-nil
+  "The result of `(select-keys m ks)` but if an element is present
+   in ks but not in m, add it with value nil. Order is not necessarily
+   retained.
 
    Example:
    (select-keys-with-nil m [:a :b :d])
     => {:b 2, :d nil, :a 1}"
-  (let [present-keys (select-keys m ks)
-        missing-keys (set/difference (set ks) (set (keys m)))]
-    (merge (apply hash-map (interleave missing-keys (cycle [nil])))
-           present-keys)))
+  [m ks]
+  (->> [nil]
+       cycle
+       (interleave (set/difference (set ks) (set (keys m))))
+       (apply hash-map)
+       (merge (select-keys m ks))))
 
+(defmacro comp+ [& args]
+  "Works like `comp` but allows you to specify the last argument as
+  input parametr to the composed function."
+  `((comp ~@(butlast args)) ~(last args)))
 
-(defmacro .+ [& rest]
-  `((comp ~@(butlast rest)) ~(last rest)))
-
-; taken from https://github.com/raynes/fs/blob/master/src/me/raynes/fs.clj
+                                        ; taken from https://github.com/raynes/fs/blob/master/src/me/raynes/fs.clj
 (defn glob->regex
   "takes a glob-format string and returns a regex."
   [s]
@@ -40,9 +58,3 @@
         (#{\. \( \) \| \+ \^ \$ \@ \%} c) (recur (next stream) (str re \\ c)
                                                  curly-depth)
         :else (recur (next stream) (str re c) curly-depth)))))
-
-(defn all [xs]
-  (every? true? xs))
-
-(defn any [xs]
-  (some true? xs))
